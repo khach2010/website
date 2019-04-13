b190572d7954c203a3a1f5a0d62d94cabf38a9d5
khach2010 <41897918+khach2010@users.noreply.github.com>
2019-04-13 14:36:12 +0200

    Updates

---
+++ precache-manifest.ed4845d1b6bfc8d24b8304c53d7ebacf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "10488fc9350d5183105a",
+    "url": "/website/static/js/main.de86bb01.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/website/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "10488fc9350d5183105a",
+    "url": "/website/static/css/main.4701a6e6.chunk.css"
+  },
+  {
+    "revision": "f92660c98950b517f1cf1d28d4df3642",
+    "url": "/website/index.html"
+  }
+];
